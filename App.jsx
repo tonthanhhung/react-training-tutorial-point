c305b0bc209e7f649d510ef1b9b196ab610d0a7a
VuHVP1 <mariohuynh0305@gmail.com>
2017-02-09 22:55:05 +0700

    init

---
+++ App.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class App extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            data:
+            [
+                {
+                    "id": 1,
+                    "name": "Foo",
+                    "age": "20"
+                },
+
+                {
+                    "id": 2,
+                    "name": "Bar",
+                    "age": "30"
+                },
+
+                {
+                    "id": 3,
+                    "name": "Baz",
+                    "age": "40"
+                }
+            ]
+        }
+    }
+
+    render() {
+        var condition = true;
+        return (
+            <div>
+                {/* This is just comment */}
+                <h1>Header</h1>
+                <h3>Content</h3>
+                <p data-name="Vu">This is a paragraph</p>
+                <p>{condition == true ? "It's true :p" : "It's not true :)"}</p>
+                <Header />
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i) => <Content key={i} data={person} />)}
+                    </tbody>
+                </table>
+                {/* Test props */}
+                <h3>Test props</h3>
+                <h4>{this.props.headerProp}</h4>
+                <h4>{this.props.contentProp}</h4>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <h1>This is header component</h1>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        );
+    }
+}
+
+export default App;
---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App headerProp = "Header from props..." contentProp = "Content from props..."/>, document.getElementById('app'));
