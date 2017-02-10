a58759c8984a711ee660902181471eca67541a92
Nguyen Thi Thuy Tien (GD-U.CMW) <TienNTT3@fsoft.com.vn>
2017-02-10 15:18:19 +0700

    TienNTT3- ReactJs Components

--- tienntt3/reactApp/App.jsx
+++ tienntt3/reactApp/App.jsx
@@ -5,16 +5,36 @@ class App extends React.Component {
       super(props);
 		
       this.state = {
-         header: "Header from state...",
-         "content": "Content from state..."
+         header: "Header from props...",
+         "content": "Content from props..."
       }
    }
 	
    render() {
       return (
          <div>
-            <h1>{this.state.header}</h1>
-            <h2>{this.state.content}</h2>
+            <Header headerProp = {this.state.header}/>
+            <Content contentProp = {this.state.content}/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>{this.props.headerProp}</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>{this.props.contentProp}</h2>
          </div>
       );
    }
