32b82ca0ee386fe284110957f79ce4a40399f89a
tiennguyen2501 <ntttien251@gmail.com>
2017-02-13 16:25:15 +0700

    TienNTT3- Move UP/ Move DOWN item

--- tienntt3/reactApp/App.jsx
+++ tienntt3/reactApp/App.jsx
@@ -1,43 +1,107 @@
 import React from 'react';
 
 class App extends React.Component {
+
    constructor(props) {
       super(props);
-		
-      this.state = {
-         header: "Header from props...",
-         "content": "Content from props..."
-      }
-   }
-	
-   render() {
-      return (
-         <div>
-            <Header headerProp = {this.state.header}/>
-            <Content contentProp = {this.state.content}/>
-         </div>
-      );
-   }
-}
+   };
 
-class Header extends React.Component {
    render() {
       return (
-         <div>
-            <h1>{this.props.headerProp}</h1>
-         </div>
+         <Content/>
       );
    }
 }
 
 class Content extends React.Component {
+ constructor(props) {
+      super(props);
+   }
+
    render() {
       return (
-         <div>
-            <h2>{this.props.contentProp}</h2>
+          <div>
+            <AddComponent/>
          </div>
       );
    }
+}
+
+class AddComponent extends React.Component{
+    constructor(props) {
+      super(props);
+		
+      this.state = {
+         arrayItem: [],
+         addValue: ""
+      }
+
+      this.setOnChangeValue = this.setOnChangeValue.bind(this);
+      this.setAddValue = this.setAddValue.bind(this);
+      this.onSwap = this.onSwap.bind(this);
+      this.onTop = this.onTop.bind(this);
+   }
+
+   setOnChangeValue(e) {
+      this.setState({addValue: e.target.value});
+      console.log(this.state.addValue);
+   }
+
+   setAddValue(event) {
+        var item = this.state.arrayItem.slice();
+        item.push(this.state.addValue);
+        this.setState({arrayItem: item});
+       
+   } 
+
+   onSwap(x, y){
+       var item = this.state.arrayItem.slice();
+        var temp = item[x];
+        item[x] = item[y];
+        item[y] = temp;
+        this.setState({arrayItem: item});
+   }
+
+   onTop(x){ 
+       var y = 0;
+       this.onSwap(x,0);    
+   }
+
+   moveUp(x){
+        if(x>0){
+            var y = x--;
+            this.onSwap(x,y);  
+        }
+   }
+   moveDown(x){
+        if(x< this.state.arrayItem.length-1){
+            var y = x++;
+            this.onSwap(x,y);   
+        }
+   }
+
+    render(){
+        return (
+            <div>
+                <input type = "text" value = {this.state.addValue} onChange = {this.setOnChangeValue}/>
+                <input type = "button" value ="Add" onClick = {this.setAddValue}/>    
+                
+                <ul>
+                    {this.state.arrayItem.map ((item , i ) =>(
+                        <li key = {i}>
+                            
+                            <span>{i} - </span>
+                            <span>{item} </span>
+                            <input type = "button" value ="Top" onClick = {() => this.onTop(i)}/> 
+                             <input type = "button" value ="Up" onClick = {() => this.moveUp(i)}/> 
+                              <input type = "button" value ="Down" onClick = {() => this.moveDown(i)}/> 
+                        </li>
+                    )
+                    )}
+                </ul>   
+            </div>        
+        );
+    }
 }
 
 export default App;
